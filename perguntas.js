97c054ec09a3c2768a2f9a2dde1ccc0d4b7e16e7
edudamello <167912251+edudamello@users.noreply.github.com>
2024-10-23 12:00:58 -0300

    flashcards

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Qual a premissa principal de The Walking Dead?',
+    'Uma serie de TV que segue um grupo de sobreviventes em um mundo pos-apocaliptico infestados por zumbis. '
+)
+
+criaCartao(
+    'Protagonismo',
+    'Qual o protagonista da serie?',
+    'O principal protagonista nas primeiras temporadas era o Rick Grimes, um ex-xerife que acaba de acordar de um coma no meio do apocalipse zumbi.'
+)
+
+criaCartao(
+    'Programação',
+    'Quem sao os walkers?',
+    'Sao os zumbis. Humanos que foram infectados e retornaram a vida depois da morte.'
+)
+
+criaCartao(
+    'Temporadas',
+    'Quantas temporadas a serie tem?',
+    'A serie tem um total de 11 temporadas, com a ultima sendo lancada em 2022. '
+)
